5623ab8146401f34823c874966d4cc859d52f34a
ravitejav <raviteja99866@gmail.com>
2022-05-17 20:59:08 +0530

    Timer component is done and updated the testcases

--- example/src/Components/ComponentPropsDisplay.tsx
+++ example/src/Components/ComponentPropsDisplay.tsx
@@ -11,7 +11,6 @@ export const ComponentPropsList = (props: ComponentPropsListProps) => {
           <li key={index} className="p-0 pb-2">
             <div className='flex flex-col bg-slate-800 text-white p-2 rounded-md'>
               <span>Property: {componentProp.name}</span>
-              {/* <span>{componentProp.syntax}</span> */}
               <CodeBlock title="sytax" code={componentProp.syntax} />
               <span>{componentProp.explanation}</span>
               <span>Optional: {componentProp.optional ? 'true':'false'}</span>
